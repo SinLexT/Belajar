b3234aa9bfedf22207e268d5724ecd6fae9a0646
Bayu Aji Setyawan <aji@MacBook-Pro.local>
2020-07-15 16:47:01 +0700

    end of section 3

--- SinLexT_/Operators/src/learnprogramming/academy/Main.java
+++ SinLexT_/Operators/src/learnprogramming/academy/Main.java
@@ -62,5 +62,33 @@ public class Main {
         if ((topScore > 90) || (secondTopScore <= 90)) {
             System.out.println("Either or both of the conditions are true");
         }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
     }
 }
